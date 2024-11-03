ca926d6b60c81f962c6063110fe334097c41da0b
gericke98 <sgerickee@gmail.com>
2024-11-03 19:22:15 +0100

    header changes

--- app/hero.tsx
+++ app/hero.tsx
@@ -16,16 +16,17 @@ export default function Hero({
         Santiago Gericke Parga
       </p>
       <h1 className="lg:text-6xl text-2xl font-bold text-white text-center">
-        From Ideas to Impactful Code <br></br>
+        Transforming Ideas into Scalable Solutions<br></br>
         <span className="lg:text-3xl text-base p-0 m-0">
-          Seeking innovative solutions that{" "}
           <span className="bg-gradient-to-r from-pink-500 via-red-500 to-purple-500 bg-clip-text text-transparent">
-            challenge the status quo
-          </span>
+            Challenging the status quo
+          </span>{" "}
+          with full-stack development, generative AI, and strategic innovation.
         </span>
       </h1>
       <p className="lg:text-lg text-sm text-slate-100 lg:mt-4 mt-1 animate-fade-in">
-        Full Stack Developer | Strategy Analyst | Entrepreneur
+        Full Stack Developer | Generative AI Specialist | Strategy Analyst |
+        Entrepreneur
       </p>
       <button
         onClick={onScrollToPath}
